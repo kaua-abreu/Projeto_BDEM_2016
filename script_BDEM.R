78725815188bee298cc3b7e8c487ea3f402eb850
kaua-abreu <kaua808player@gmail.com>
2026-09-06 01:49:08 -0300

    script BDEM - SIM - tarefa 1

--- script_BDEM.R
+++ script_BDEM.R
@@ -16,6 +16,7 @@
 # Tarefa 1. Leitura do banco de dados SIM_2016 com 1309774 linhas e 87 colunas com o nome de dados_sim
 # Verificar se a leitura foi feita corretamente e a estrutura dos dados
 
+dados_sim = read.csv("SIM_2016.csv", header = TRUE, sep=";")
 
 # Ao terminar a Tarefa 1 commit com a mensagem "script BDEM - SIM - tarefa 1" e envie para o repositório Projeto_BDEM_2016
 
